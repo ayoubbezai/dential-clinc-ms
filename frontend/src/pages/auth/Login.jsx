9beb093206f9be34facbfda8f39ec8e7bd7acb46
ayoubbezai <ayoubbezai05@gmail.com>
2025-03-13 02:35:11 +0100

    fix front end

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { useRole } from "../../hooks/useRole";
-
+import LoginImage from "../../assets/images/auth/473812939_988438703349267_4731828135405535889_n.jpg"
 const Login = () => {
     const { login, error } = useAuth();
     const role = useRole();
@@ -17,18 +17,22 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <p>Role: {role}</p> {/* ✅ Role comes from useRole() */}
+        <div className="flex">
+            <div className="w-1/3">
+                <img src={LoginImage} alt="" />
+            </div>
+            <div className="w-1/2">
+                <h2>Login</h2>
+                {error && <p style={{ color: "red" }}>{error.message || "Login failed."}</p>}
+                {error?.errors?.email && <p style={{ color: "red" }}>{error.errors.email[0]}</p>}
 
-            {error && <p style={{ color: "red" }}>{error.message || "Login failed."}</p>}
-            {error?.errors?.email && <p style={{ color: "red" }}>{error.errors.email[0]}</p>}
+                <form onSubmit={handleSubmitLogin}>
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input ref={passwordRef} type="password" placeholder="Password" />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
 
-            <form onSubmit={handleSubmitLogin}>
-                <input ref={emailRef} type="email" placeholder="Email" />
-                <input ref={passwordRef} type="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </form>
         </div>
     );
 };
