ad96cbe4f11fa40f12ef9796af177db391239ca6
ayoubbezai <ayoubbezai05@gmail.com>
2025-03-13 01:36:13 +0100

    auth front end

--- frontend/src/hooks/useRole.jsx
+++ frontend/src/hooks/useRole.jsx
@@ -3,5 +3,5 @@ import { useAuth } from "./useAuth";
 export const useRole = () => {
     const { user } = useAuth();
 
-    return user["role"] ||null;
+    return user ? user["role"] : "no role";
 };
