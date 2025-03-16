5cd89e4f38ade6b36b706c373a683b8937b80c04
ayoubbezai <bezaiayoub8@gmail.com>
2025-03-16 03:54:20 +0100

    fix login frontend

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -18,11 +18,25 @@ const Login = () => {
     } = useLogin();
 
     return (
-        <div className="flex bg-primary">
+        <div className={`flex  md:bg-primary`}
+            style={{
+                backgroundImage: `url(${LoginImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "left",
+            }}>
+            <style>
+                {`
+      @media (min-width: 767px) {
+        div {
+          background-image: none !important;
+        }
+      }
+    `}
+            </style>
             <div className="w-1/2 hidden md:flex ">
                 <img src={LoginImage} alt="" className="hidden md:flex h-screen " />
             </div>
-            <div className="md:w-2/3 flex flex-col md:justify-center w-full items-center ">
+            <div className="md:w-2/3 min-h-[100vh] flex flex-col md:justify-center w-full items-center ">
                 <img src={logo} alt="logo" className="w-52 mb-8   " />
                 <div className="bg-white  rounded-2xl p-8 w-100 px-12">
                     <h2 className="font-bold text-xl my-3">Login to your account</h2>
