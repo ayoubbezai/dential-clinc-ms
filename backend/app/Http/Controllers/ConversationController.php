4b5c893e121c6bbc6235199362d92cb392e55661
ayoubbezai <bezaiayoub8@gmail.com>
2025-04-29 02:42:34 +0100

    conversations backend and frontend

--- backend/app/Http/Controllers/ConversationController.php
+++ backend/app/Http/Controllers/ConversationController.php
@@ -63,6 +63,7 @@ class ConversationController extends Controller
     {
         try {
             $requestQuery = $request->query();
+            $lastPosition = $requestQuery["lastPosition"] ?: 0;
             $perPage = filter_var($requestQuery['per_page'] ?? 15, FILTER_VALIDATE_INT) ?: 15;
             $perPage = max($perPage, 1);
 
@@ -96,7 +97,8 @@ class ConversationController extends Controller
                 })
                 ->whereDoesntHave('conversation')
                 ->limit($missingCount)
-                ->with('role:id,name') 
+                ->with('role:id,name')
+                ->startWith($lastPosition)
                 ->get();
 
                 foreach ($usersWithoutConversation as $user) {
